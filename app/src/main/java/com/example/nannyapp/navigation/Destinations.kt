b34a33fb3a053f0c78189ca4aa5f986673bb83f7
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-31 23:19:08 -0600

    Destinations con Login, Register y Home

--- app/src/main/java/com/example/nannyapp/navigation/Destinations.kt
+++ app/src/main/java/com/example/nannyapp/navigation/Destinations.kt
@@ -1,18 +1,45 @@
 package com.example.nannyapp.navigation
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import com.example.nannyapp.R
 
 sealed class Destinations(
-val route: String,
-val title: String,
-val icon: Int
-){
-    object Alimentos: Destinations("alimentos", "Alimentos", R.drawable.biberon)
-    object Otros: Destinations("otros", "Otros", R.drawable.otros){
+    val route: String,
+    val title: String,
+    val icon: Int,
+    val arguments: List<NamedNavArgument>
+) {
+    object Alimentos : Destinations("alimentos", "Alimentos", R.drawable.biberon)
+    object Otros : Destinations("otros", "Otros", R.drawable.otros) {
         fun createRoute(newText: String) = "otros/?newText={newText}=$newText"
     }
-    object Panales: Destinations("panales", "Panales", R.drawable.panal)
-    object Siestas: Destinations("siestas", "Siestas", R.drawable.siesta)
-    object Home: Destinations("Menú", "Menú", R.drawable.home)
+
+    object Panales : Destinations("panales", "Panales", R.drawable.panal)
+    object Siestas : Destinations("siestas", "Siestas", R.drawable.siesta)
+    object Home : Destinations(
+        route = "home",
+        arguments = listOf(
+            navArgument("email") { type = NavType.StringType },
+            navArgument("password") { type = NavType.StringType }
+        ),
+        title = "Menú",
+        icon = R.drawable.home
+    )
+
+    object Login : Destinations(
+        route = "login",
+        arguments = emptyList(),
+        icon = R.drawable.home,
+        title = "Iniciar sesión"
+    )
+
+    object Register : Destinations(
+        route = "register",
+        arguments = emptyList(),
+        icon = R.drawable.home,
+        title = "Crear cuenta"
+    )
 }
