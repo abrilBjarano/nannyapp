d6ef3c4e9abc1567755c56108f59bd47fe503384
Jesús Andrés Mata <andresmata.c21@gmail.com>
2022-10-31 23:24:57 -0600

    Fix: Argumentos y rutas eliminados

--- app/src/main/java/com/example/nannyapp/navigation/Destinations.kt
+++ app/src/main/java/com/example/nannyapp/navigation/Destinations.kt
@@ -10,36 +10,15 @@ sealed class Destinations(
     val route: String,
     val title: String,
     val icon: Int,
-    val arguments: List<NamedNavArgument>
 ) {
     object Alimentos : Destinations("alimentos", "Alimentos", R.drawable.biberon, emptyList())
-    object Otros : Destinations("otros", "Otros", R.drawable.otros, emptyList()) {
+    object Otros : Destinations("otros", "Otros", R.drawable.otros) {
         fun createRoute(newText: String) = "otros/?newText={newText}=$newText"
     }
 
-    object Panales : Destinations("panales", "Panales", R.drawable.panal, emptyList())
-    object Siestas : Destinations("siestas", "Siestas", R.drawable.siesta, emptyList())
-    object Home : Destinations(
-        route = "home",
-        arguments = listOf(
-            navArgument("email") { type = NavType.StringType },
-            navArgument("password") { type = NavType.StringType }
-        ),
-        title = "Menú",
-        icon = R.drawable.home
-    )
-
-    object Login : Destinations(
-        route = "login",
-        arguments = emptyList(),
-        icon = R.drawable.home,
-        title = "Iniciar sesión"
-    )
-
-    object Register : Destinations(
-        route = "register",
-        arguments = emptyList(),
-        icon = R.drawable.home,
-        title = "Crear cuenta"
-    )
+    object Panales : Destinations("panales", "Panales", R.drawable.panal)
+    object Siestas : Destinations("siestas", "Siestas", R.drawable.siesta)
+    object Home : Destinations("Menú", "Menú", R.drawable.home)
+    object Login : Destinations("login", "Iniciar sesión", R.drawable.home)
+    object Register : Destinations("register", "Crear cuenta", R.drawable.home)
 }
